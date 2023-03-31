4fc89d4efda0ba73b1514cb0ba352fb99df2cf37
Pedram <shemuel1226@gmail.com>
2023-03-31 22:48:08 +0430

    until session 80

--- src/components/formikComponents/FormikControl.jsx
+++ src/components/formikComponents/FormikControl.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import Input from './Input';
+import React from "react";
+import Input from "./Input";
+import Radio from "./Radio";
+import Date from './Date';
 
 const Formikcontrol = (props) => {
-    switch (props.control) {
-        case 'input':
-            return <Input {...props}/>
-        default:
-            return null
-    }
-}
+  switch (props.control) {
+    case "input":
+      return <Input {...props} />;
+    case "radio":
+      return <Radio {...props} />;
+    case "date":
+      return <Date {...props} />;
+    default:
+      return null;
+  }
+};
 
 export default Formikcontrol;
---
+++ src/components/formikComponents/Radio.jsx
@@ -0,0 +1,33 @@
+import { ErrorMessage, FastField } from 'formik';
+import React, { Fragment } from 'react';
+// import Personalerror from '../Personalerror';
+
+const Radio = (props) => {
+    const {name,label,options} = props;
+    return (
+        <div className="mb-2">
+            <label htmlFor={name} className="form-label">{label}</label>
+            <FastField className="form-control" id={name} name={name}>
+                {({field})=>{
+                    return options.map(o=>(
+                        <Fragment key={o.id}>
+                            <input 
+                            className='form-check-input mx-2'
+                            type="radio" 
+                            id={o.id}
+                            {...field}
+                            value={o.id}
+                            checked={field.value == o.id}
+                            />
+                            <label htmlFor={o.id} 
+                            className="mx-1 mx-4">{o.value}</label>
+                        </Fragment>
+                    ))
+                }}
+            </FastField>
+            {/* <ErrorMessage name={name}component={Personalerror} /> */}
+        </div>
+    );
+}
+
+export default Radio;
